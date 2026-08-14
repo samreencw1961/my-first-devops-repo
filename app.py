0773e5108f76bebaea4aaa886fb88defa8be483a
samreencw <samreensameya@gmail.com>
2026-08-14 15:12:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    #return a - b #intentionally broke
+    return a + b #corrected back
+
+print(add(2, 3))
---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+
+print("All tests passed")
